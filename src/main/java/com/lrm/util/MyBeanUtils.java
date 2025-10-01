44e3394279dfd825f7d189f1a0c72eda32604024
Qinhao Chang <qinhaochang@Mac.attlocal.net>
2025-10-01 11:37:51 -0700

    first commit

---
+++ src/main/java/com/lrm/util/MyBeanUtils.java
@@ -0,0 +1,34 @@
+package com.lrm.util;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.beans.PropertyDescriptor;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by limi on 2017/10/21.
+ */
+public class MyBeanUtils {
+
+
+    /**
+     * Get array of all property names with null values
+     * @param source
+     * @return
+     */
+    public static String[] getNullPropertyNames(Object source) {
+        BeanWrapper beanWrapper = new BeanWrapperImpl(source);
+        PropertyDescriptor[] pds =  beanWrapper.getPropertyDescriptors();
+        List<String> nullPropertyNames = new ArrayList<>();
+        for (PropertyDescriptor pd : pds) {
+            String propertyName = pd.getName();
+            if (beanWrapper.getPropertyValue(propertyName) == null) {
+                nullPropertyNames.add(propertyName);
+            }
+        }
+        return nullPropertyNames.toArray(new String[nullPropertyNames.size()]);
+    }
+
+}
